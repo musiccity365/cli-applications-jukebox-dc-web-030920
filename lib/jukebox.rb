10f86bf7484e3d9ea18d3158a49b0f5bb1f8f288
Michael David Sackstein <m_sackstein@yahoo.com>
2020-03-05 22:44:33 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,14 +1,5 @@
 # Add your code here
-=begin
-def say_hello(name)
-  "Hi #{name}!"
-end
- 
-puts "Enter your name:"
-users_name = gets.strip
- 
-puts say_hello(users_name)
-=end
+require 'pry'
 
 songs = [
   "Phoenix - 1901",
