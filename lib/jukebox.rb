27bee09c3b6bc096360d651cc2a071aaee15ca6c
Michael David Sackstein <m_sackstein@yahoo.com>
2020-03-05 22:38:26 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -9,3 +9,11 @@ users_name = gets.strip
  
 puts say_hello(users_name)
 =end
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
